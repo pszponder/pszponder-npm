f760baa18abf603f6823ecb0f75ae53a51f07daa
Piotr Szponder <14116413+pszponder@users.noreply.github.com>
2022-08-15 12:04:37 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+console.log(`
+Piotr Szponder
+Fullstack Software Developer
+
+Hi, I'm Piotr Szponder, it's great to meet you!
+Thanks for checkout out my CLI app!
+
+GitHub: https://github.com/pszponder
+`);
