91e064a7af1a8cb8ab653a7b1a38b87cad44a4e0
Piotr Szponder <14116413+pszponder@users.noreply.github.com>
2023-02-05 09:41:28 -0700

    feat: add method to clear console before printing bio

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 #!/usr/bin/env node
+
+/**
+ * Cross platform clear console
+ * Platform: macOS, Linux, and Windows
+ * Source -- https://www.npmjs.com/package/clear-any-console
+ */
+const clearConsole = () =>
+  process.stdout.write(
+    process.platform === 'win32' ? '\x1B[2J\x1B[0f' : '\x1B[2J\x1B[3J\x1B[H'
+  );
+
+// Clear the console
+clearConsole();
+
+// Display the user bio in the conosle
 console.log(`
 Piotr Szponder
 Fullstack Software Developer
